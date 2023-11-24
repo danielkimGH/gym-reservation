ebb7dab46a5939c2566daf3ae7677112c433154c
Daniel Kim <kimda6@flip3.engr.oregonstate.edu>
2023-11-23 20:38:02 -0800

    Added court routes for app.py and pages for courts. Edit court functionality is still not working properly and is a work in progress. Updates to gyms and members templates pertainted to formats only and no changes to content

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 
 # Database connection information
 app.config["MYSQL_HOST"] = "classmysql.engr.oregonstate.edu"
-app.config["MYSQL_USER"] = "cs340_hsiangb"
-app.config["MYSQL_PASSWORD"] = "2174"
-app.config["MYSQL_DB"] = "cs340_hsiangb"
+app.config["MYSQL_USER"] = "cs340_kimda6"
+app.config["MYSQL_PASSWORD"] = "2371"
+app.config["MYSQL_DB"] = "cs340_kimda6"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
@@ -36,13 +36,6 @@ def gyms():
         cur.execute(query)
         data = cur.fetchall()
 
-        # Query to grab gym location for dropdown
-        # query2 = "select id, location from Gyms"
-        # cur = mysql.connection.cursor()
-        # cur.execute(query2)
-        # gym_location_data = cur.fetchall()
-
-        # return render_template("gyms.j2", data = data, gym_location = gym_location_data)
         return render_template("gyms.j2", data = data)
 
     # Add a new gym
@@ -82,7 +75,7 @@ def edit_gym(id):
         cur.execute(query)
         data = cur.fetchall() 
     
-    # dropdowwn code here 
+    # dropdown code here 
 
         return render_template("edit_gyms.j2", data=data)
     
@@ -105,6 +98,8 @@ def edit_gym(id):
             
         return redirect("/gyms")
 
+
+# Route for Members page
 @app.route("/members", methods = ["POST", "GET"])
 def members():
     if request.method == "GET":
@@ -169,7 +164,79 @@ def edit_member(id):
 
         return redirect("/members")
 
-    
+
+# Route for Courts page
+@app.route("/courts", methods = ["POST", "GET"])
+def courts():
+    if request.method == "GET":
+        query = "select * from Courts order by gym_ID asc"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # Query to get a dropdown list of gym locations
+        query2 = "select gym_ID, location from Gyms"
+        cur = mysql.connection.cursor()
+        cur.execute(query2)
+        gym_location_data = cur.fetchall()
+
+        return render_template("courts.j2", data=data, locations=gym_location_data)
+
+    if request.method == 'POST': 
+        if request.form.get("Add_Court"): 
+            gym_ID =request.form["gym_ID"]
+            # gym_location = request.form["gym_location"]
+            court_name = request.form["court_name"]
+
+            query = "insert into Courts (gym_ID, court_name) values (%s, %s)"
+            # query = "insert into Courts (gym_ID, court_name) values ((select gym_ID from Gyms where Gyms.location=%s), %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (gym_ID, court_name))
+            # cur.execute(query, (gym_location, court_name))
+            mysql.connection.commit()
+
+            return redirect("/courts")
+
+@app.route("/edit_court/<int:id>", methods=["POST", "GET"])
+def edit_court(id):
+    if request.method == 'GET':
+        # SQL query to get info of Courts from court ID
+        query = "SELECT * from Courts where court_ID = %s" % (id)
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall() 
+
+        # SQL query to get dropdown of gym locations
+        query2 = "SELECT gym_ID, location from Gyms"
+        cur = mysql.connection.cursor()
+        cur.execute(query2)
+        gym_location_data = cur.fetchall()
+
+        # Return edit_courts page passing query data and gym location data to the edit_courts page
+        return render_template("edit_courts.j2", data=data, locations=gym_location_data)
+
+    if request.method == "POST":
+        if request.form.get("edit_court"):
+            id = request.form["court_ID"]
+            location = request.form["gym_location"]
+            court_name = request.form["court_name"]
+
+            # query = "update Courts set Courts.gym_ID = (SELECT gym_ID FROM Gyms where Gyms.location = %s), Courts.court_name = %s where court_ID = %s"
+            query = "update Courts set Courts.gym_ID = Courts.gym_ID = %s, Courts.court_name = %s where court_ID = %s"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (location, court_name, id))
+            mysql.connection.commit()
+
+        return redirect("/courts")
+
+# @app.route("/delete_court/<int:id>")
+# def delete_court(id):
+#     query = "DELETE from Courts where court_ID = %s;"
+#     cur = mysql.connection.cursor()
+#     cur.execute(query, (id,))
+#     mysql.connection.commit()
+
+#     return redirect("/courts")
 
 
 if __name__ == "__main__":
